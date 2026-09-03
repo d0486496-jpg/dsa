85ec0a5b88ea92d06b50c3fc661cf4465073ab38
d0486496-jpg <d0486496@gmail.com>
2026-09-03 15:12:42 +0530

    Time: 239 ms (14.69%), Space: 12.9 MB (59.5%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        new=""
+        for i in s:
+            if i.isalnum():
+                new +=i.lower()
+        return new==new[::-1]
+    
+        
+
+        
